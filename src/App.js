c68cfe9822d7543fe113adc7c84f138f2bed4762
sokolovalova <anisok170800@gmail.com>
2020-08-31 12:23:00 +0300

    react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  './gadget.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import FirstView  from './render-components/FirstView.js'
+
+
+export class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      selectedView: 0,
+      seconds: 0,
+      minutes:0,
+      hours:0,
+      day: 0,
+      month:0,
+      year:0,
+      NameMonth: 0
+    }
+  }
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        seconds: (new Date()).getSeconds(),
+        minutes: (new Date()).getMinutes(),
+        hours:  (new Date()).getHours(),
+        day: (new Date()).getDate(),
+        month: (new Date()).getMonth()+1,
+        year: (new Date()).getFullYear(),
+        NameMonth: (new Date()).toLocaleString('En', { month: 'long' })
+
+      })
+    }, 1000)
+  }
+
+
+  renderClock(){
+    switch (this.state.selectedView) {
+      case 0:
+         return <div className="view">{`${this.state.hours}:${this.state.minutes}:${this.state.seconds}`+` ${this.state.hours<12?"AM":"PM"}`}</div>
+      case 1:
+         return <div  className="view">{`${this.state.day}/${this.state.month}/${this.state.year}`}</div>;
+      case 2:
+         return(
+          <div>
+           <div  className=" view third">{`${this.state.NameMonth} ${this.state.day}th ${(this.state.year).toString().slice(2)}`}</div>
+          </div>);
+      case 3:
+        return <div className="view">{`${this.state.hours}:${this.state.minutes}`+` ${this.state.hours<12?"AM":"PM"}`}</div>
+      default:
+        break;
+    }
+  }
+
+  changeView=()=>{
+   
+    let view = this.state.selectedView < 3 ? this.state.selectedView + 1 : 0;
+    this.setState({
+      selectedView: view
+    })
+  }
+  render(){
+    return( 
+        <div onClick={this.changeView} className={`gadget col_${this.state.selectedView + 1}`}>
+          {this.renderClock()}
+          
+        </div>);
+  }
 }
 
 export default App;
