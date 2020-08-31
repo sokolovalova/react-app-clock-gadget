082c8df9574878f7f947cf27d3ad96fe0594cf60
sokolovalova <anisok170800@gmail.com>
2020-08-31 12:23:57 +0300

    Updates

---
+++ precache-manifest.1d498822e503207179ab5084e0ed9178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525611bc9d193617a76bc9fe1b17f545",
+    "url": "/react-app-clock-gadget/index.html"
+  },
+  {
+    "revision": "065be229d02a0538c3c5",
+    "url": "/react-app-clock-gadget/static/css/main.17804b36.chunk.css"
+  },
+  {
+    "revision": "7f37a417ed9856212ad3",
+    "url": "/react-app-clock-gadget/static/js/2.f9baf550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-clock-gadget/static/js/2.f9baf550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065be229d02a0538c3c5",
+    "url": "/react-app-clock-gadget/static/js/main.51decc12.chunk.js"
+  },
+  {
+    "revision": "53b4715d0b1a6bd3ac43",
+    "url": "/react-app-clock-gadget/static/js/runtime-main.608a69ac.js"
+  }
+]);
